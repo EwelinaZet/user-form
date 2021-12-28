121db658619e3a3c0da047ad92cccbe5c6faae33
Ewelina <ewelinazwolenik421@gmail.com>
2021-12-28 22:43:47 +0100

    add form validation

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -1,21 +1,25 @@
 <template>
-  <form class="form">
-    <h2 class="form-title">Contact Us</h2>
-    <div class="form-control">
-      <label class="input-label">Name</label>
-      <input class='input-field' name='name' type='text' v-model.trim='name'/>
+  <form class='form'>
+    <h2 class='form-title'>Contact Us</h2>
+    <div class='form-control' :class='{invalid: nameValidity}'>
+      <label class='input-label'>Name</label>
+      <input class='input-field' name='name' type='text' v-model.trim='name' @blur='validateNameInput' required/>
+      <p class='input-notificatioin' v-if='nameValidity'>Username cannot be blank and has at least 25 characters and cannot be longer than 50 characters.</p>
     </div>
-    <div class="form-control">
-      <label class="input-label">E-mail</label>
-      <input class='input-field' name='email' type='text' v-model.trim='email'/>
+    <div class='form-control' :class='{invalid: emailValidity}'>
+      <label class='input-label'>E-mail</label>
+      <input class='input-field' name='email' type='text' v-model.trim='email' @blur='validateEmailInput' required/>
+      <p class='input-notificatioin' v-if='emailValidity'>Invalid email value.</p>
     </div>
-    <div class="form-control">
-      <label class="input-label">Subject</label>
-      <input class='input-field' name='subject' type='text' v-model.trim='subject'/>
+    <div class='form-control' :class='{invalid: subjectValidity}'>
+      <label class='input-label'>Subject</label>
+      <input class='input-field' name='subject' type='text' v-model.trim='subject' @blur='validateSubjectInput'/>
+      <p class='input-notificatioin' v-if='subjectValidity'>Subject cannot be longer than 100 characters.</p>
     </div>
-    <div class="form-control">
-      <label class="input-label">Message</label>
-      <textarea row='20' placeholder='Your message here...' class='input-field' name='message' v-model.trim='message'/>
+    <div class='form-control' :class='{invalid: messageValidity}'>
+      <label class='input-label'>Message</label>
+      <textarea row='20' placeholder='Your message here...' class='input-field' name='message' type='text-area' v-model.trim='message' @blur='validateMessageInput' required/>
+      <p class='input-notificatioin' v-if='messageValidity'>Message cannot be blank and cannot be longer than 500 characters.</p>
     </div>
     <div>
         <button>
@@ -39,6 +43,47 @@ export default class TheForm extends Vue {
   subject=''
 
   message=''
+
+  nameValidity = false
+
+  emailValidity = false
+
+  subjectValidity = false
+
+  messageValidity = false
+
+  validateNameInput(): void {
+    if(this.name.length < 5 || this.name.length > 50 || this.name.length === 0) {
+      this.nameValidity = true
+    } else {
+      this.nameValidity = false
+    }
+  }
+
+  validateEmailInput(): void {
+    const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if(this.email.length === 0 || !this.email.match(pattern)) {
+      this.emailValidity = true
+    } else {
+      this.emailValidity = false
+    }
+  }
+
+  validateSubjectInput(): void {
+    if(this.subject.length > 100) {
+      this.subjectValidity = true
+    } else {
+      this.subjectValidity = false
+    }
+  }
+    
+  validateMessageInput(): void {
+    if(this.name.length === 0 || this.name.length > 500) {
+      this.messageValidity = true
+    } else {
+      this.messageValidity = false
+    }
+  }
 }
 </script>
 
